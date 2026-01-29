3251f22b0dfdaf4278acac93f5146f6d1a398bfa
Morfy <overlord4527@gmail.com>
2026-01-29 18:49:11 +0300

    Add files via upload

--- iou.py
+++ iou.py
@@ -1,230 +1,102 @@
 import time
 import random
-class Sim:
+class Resident:
     def __init__(self, name):
         self.name = name
-        self.hunger = 50
-        self.energy = 100
+        self.hunger = 60
+        self.energy = 80
         self.is_alive = True
-    def eat(self):
-        if self.hunger >= 100:
-            print(f"{self.name} не хочет есть.")
-        else:
-            self.hunger += 20
-            self.energy -= 5
-            print(f"{self.name} поел(а). Голод: {self.hunger}")
-    def live_dey(self):
-        self.hunger -= 10
-        self.energy -= 10
-        if self.hunger <= 0 or self.energy <= 0:
-            self.is_alive = False
-            print(f"{self.name} не выжержал суровой жизни и покинул чат.")
+
     def status(self):
         return f"{self.name} | Голод: {self.hunger} | энергия: {self.energy}"
-################################################################################################################
-class Human(Sim):
-    def __init__(self, name, job):
-        super().__init__(name)
-        self.job = job
-        self.money = 50
-    def work(self):
-        self.energy -= 30
-        self.hunger -= 20
-        self.money += 100
-        print(f"{self.name} сходил на работу ({self.job}). +100$. Энергия: {self.energy}")
-    def feed_pet(self, pet):
-        if self.money >= 20:
-            print(f"{self.name} покупает корм и кормит {pet.name}...")
-            self.money -= 20
-            pet.eat()
-        else:
-            print(f" у  {self.name} нет денег на корм! Иди работай!")
-    def feed_zombie(self, pet):
-        if self.money >= 25:
-            print(f"{self.name} покупает корм и кормит {zombie.name}...")
-            self.money -= 25
-            zombie.eat()
-        else:
-            print(f" у  {self.name} нет денег на еду! беги!!!!")
-    def feed_pet_fish(self, pet_fish):
-        if self.money >= 20:
-            print(f"{self.name} покупает корм и кормит {pet_fish.name}...")
-            self.money -= 20
-            pet_fish.eat()
-        else:
-            print(f" у  {self.name} нет денег на корм для рыбок! Иди работай!")
-    def repair_robot(self, robot):
-        print(f"{self.name} чинит {robot.name}...")
-        self.energy -= 20
-        robot.energy = 100
-        print(f"{robot.name} полностью заряжен!")
-##################################################################################################################
-class Dog(Sim):
-    def eat(self):
-        self.hunger += 30
-        print(f"🐶{self.name} жадно грызет кость! Гав!")
-    def play(self,human):
-        print(f"{self.name} приносит мячик {human.name}.")
-        self.energy -= 20
-        human.energy += 10
-        print(f'{human.name} повеселел!')
-###############################################################################################################
-class Cat(Sim):
-    def __init__(self, name):
-        super().__init__(name)
-        self.lives = 9
 
-    def eat(self):
-        self.hunger += 30
-        print(f"🐈{self.name}: Мяу! Рыбка! ")
-    def play(self,human):
-        print(f"{self.name} играет с {human.name}.")
-        self.energy -= 20
-        human.energy += 10
-        print(f'{human.name} повеселел!')
-    def tear_sofa(self):
-        self.energy -= 10
-        print(f'🐈 {self.name} подрал диван, человек будет в ярости')
-
-###############################################################################################################
-class pet_fish(Sim):
-    def eat(self):
-        self.hunger += 20
-        print(f"🐠 {self.name} ест плавующий корм")
-
-    def play(self, human):
-        print(f"{self.name} радует своей красатой {human.name}.")
+    def live_dey(self):
+        self.hunger -= 15
         self.energy -= 15
-        human.energy += 5
-        print(f"{human.name} повеселел")
-##################################################################################################################
-class Robot(Sim):
-    def __init__(self, name):
+        if self.hunger <= 0 or self.energy <= 0:
+            self.is_alive = False
+    def react_to_mess(self):
+        pass
+class WorkerSim (Resident):
+    def _init_(self, name, money):
         super().__init__(name)
-        self.battery = 100
-    def life_day(self):
-        self.energy -= 5
-    def eat(self):
-        print(f"{self.name} подключается к розетке. Зарядка...")
-        self.energy = 100
-    def cook_dinner(self,human):
-        if self.energy > 20:
-            print(f"{self.name} готовит ужин для {human.name}")
-            self.energy -= 20
-            human.eat()
-        else:
-            print(f"{self.name} : БАТАРЕЯ РАЗРЯЖЕНА. НЕ МОГУ ГОТОВИТЬ.")
-#############################################################################################################
-class Zombie(Sim):
-    def eat(self):
-        self.hunger += 30
-        print(f"{self.name} жадно ест еду")
-    def life_day(self):
-        self.hunger -= 10
-    def looking_for_food(self,human):
-        if self.energy > 20:
-            print(f"🧟‍{self.name} не обращяет внимания на {human.name}")
-        else:
-            print(f"🧟‍{self.name} атакует {human.name}")
-            human.energy -= 1000
-            zombie.hunger += 80
-#################################################################################################################
-class robot_vacuum_cleaner(Sim):
-    def __init__(self, name):
+        self.money = money
+    def work(self):
+        print(f" {self.name} пашет на двух работах! +150$")
+        self.money += 150
+        self.energy -= 40
+        self.hunger -= 20
+    def react_to_mess(self):
+        print(f" {self.name}: Опять этот мусор! Сил моих нет! (Минус 20 настроения)")
+        self.energy -= 10
+class LazySim(Resident):
+    def _init__(self, name):
         super().__init__(name)
-        self.battery = 100
-    def life_day(self):
+        self.laziness = 100
+    def live_day(self):
+        self.hunger -= 5
         self.energy -= 5
-    def eat(self):
-        print(f"{self.name} подключается к розетке. Зарядка...")
-        self.energy = 100
-    def cleans_the_house(self,human):
-        if self.energy > 20:
-            print(f"{self.name} уберает дом  для {human.name}")
-            self.energy -= 20
-            human.energy += 5
-            print(f"{human.name} повеселел")
-        else:
-            print(f"{self.name} : БАТАРЕЯ РАЗРЯЖЕНА. НЕ МОГУ Убирать.")
-###############################################################################################################
-player = Human("Алекс", "Програмист")
-doggo = Dog("Бобик")
-pet_fish = pet_fish("Аркадий")
-robo = Robot("робо_дима")
-robot_vacuum_cleaner = robot_vacuum_cleaner("Элеонора Андреевна")
-barsik = Cat("барсик")
-zombie = Zombie("Дэйв")
-household = [player, doggo, pet_fish, robo, robot_vacuum_cleaner, barsik, zombie]
+        if self.hunger <= 0 or self.energy <= 0:
+            self.is_alive = False
+    def sleep_on_trash (self):
+        print(f" {self.name} прилег на гору грязного белья. Удобно!")
+        self.energy += 40
+        self.hunger -= 5
+    def react_to_mess(self):
+        print(f"😋 {self.name}: 'Какой мусор? Это же элементы интерьера...'")
+worker = WorkerSim("Артур (работяга)", 1003)
+lazy_guy = LazySim("Ларри (Ленц)")
+roommantes = [worker, lazy_guy]
 day = 1
-print("ДОБРО ПОЖАЛОВАТЬ В SIMS: PYTHON EDITION")
+is_messy = True
+print("СИМУЛЯТОР КОММУНАЛКИ ЗАПУЩЕН")
 while True:
-    print(f"\n ДЕНЬ {day} ===")
-    game_over =False
-    for sim in household:
-        if not sim.is_alive:
-            print(f"GAME OVER: {sim.name} погиб")
-            game_over = True
-    if game_over:
+    print(f"\n=== ДЕНЬ {day} ===")
+    if not all(r.is_alive for r in roommantes):
+        dead = next(r for r in roommantes if not r.is_alive)
+        print(f"КОНЕЦ ИГРЫ: {dead.name} покинул этот мир.")
         break
-    print(f"Деньги: {player.money}$")
-    for sim in household:
-        print(sim.status())
-    print("\n Что будет делать алекс?")
-    print("1. Пойти на работу")
-    print("2. Поесть самому (-20$ еда)")
-    print("3. Покормить Бобика (-20$ корм)")
-    print("4. Поиграть с Бобиком")
-    print("5. Попросить робота приготовить ужин (Бесплатно)")
-    print("6. Починить робота")
-    print("7. Покормить Аркадия (-20$ корм)")
-    print("8. Поиграть с Аркадием")
-    print("9. Попросить Элеонору Андреевну убрать дом (Бесплатно)")
-    print("10. Починить Элеонору Андреевну")
-    print("11. Покормить Барсика (-20$ корм)")
-    print("12. Поиграть с Барсиком")
-    print("13.Барсик подрет диван")
-    print("14 покормить зомби")
+    print(f"Общак Артура: {worker.money}$ | Состояние: {'Хлам' if is_messy else 'Чисто'}")
+    for r in roommantes:
+        print(r.status())
+    print("\nВаш выбор:")
+    print("1. Артур: Пойти на работу (+150)")
+    print("2. Артур: Купить пиццу на всех")
+    print("3. Ларри: Поспвть в куче хлама (+энегия)")
+    print("4. Ларри: Попросить у Артура денег на еду")
+    print("5. Убраться в комнате (Артур тратит энергию)")
     print("0. Выход")
-    choice = input("Твой выбор")
+    choice = input("Действие:")
     if choice == "1":
-        player.work()
+        worker.work()
     elif choice == "2":
-        if player.money >= 20:
-            player.money -= 20
-            player.eat()
-        else:
-            print("Нет денег!")
-    elif choice == "3":
-        player.feed_pet(doggo)
-    elif choice == "4":
-        doggo.play(player)
-    elif choice == "5":
-        robo.cook_dinner(player)
-    elif choice == "6":
-        player.repair_robot(robo)
-    elif choice == "7":
-        player.feed_pet(pet_fish)
-    elif choice == "8":
-        pet_fish.play(player)
-    elif choice == "9":
-        robot_vacuum_cleaner.cleans_the_house(player)
-    elif choice == "10":
-        player.repair_robot(robot_vacuum_cleaner)
-    elif choice == "11":
-        player.feed_pet(barsik)
-    elif choice == "12":
-        barsik.play(player)
-    elif choice == "13":
-        barsik.tear_sofa()
-    elif choice == "14":
-        player.feed_zombie(zombie)
-    elif choice == "0":
-        print("Пока!")
-        break
-    else:
-        print("Неверная команда, день прошел впустую...")
-    print("\n Наступает ночь... Все показатели падают.")
-    time.sleep(1)
-    for sim in household:
-        sim.live_dey()
-    day += 1
+        if worker.money >= 50:
+            worker.money -= 50
+            for r in roommantes:
+                r.hunger = min(100, r.hunger + 40)
+                print("Пицца заказана! Все поели.")
+            else:
+                print("Нет денег на пиццу!")
+        elif choice == "3":
+            if is_messy:
+                lazy_guy.sleep_on_trash()
+            else:
+                print("Хлама нет, пришлось спать на голом полу. Спина болит.")
+                lazy_guy.energy += 10
+        elif choice == "4":
+            print(f"Ларри клянчит деньги. Артур в ярости!")
+            worker.energy -= 5
+        elif choice == "5":
+            print(f"Артур вынес 40 мешков мусора.")
+            is_messy = False
+            worker.energy -= 30
+        elif choice == "0":
+            break
+        print("\n Ночь в комуналке...")
+        time.sleep(1)
+        for r in roommantes:
+            r.live_day()
+            if is_messy:
+                r.react_to_mess()
+        day += 1
+print("Игра завершена.")
